8322203484128598d52a83dd2335f31bc46adfe4
GTea <grace.tveit@gmail.com>
2026-07-28 17:06:11 -0700

    adding general state

---
+++ src/components/state/state.gd
@@ -0,0 +1,35 @@
+# @tool, @icon, @static_unload
+class_name State
+extends RefCounted
+# ## doc comment
+
+# signals
+signal leave_state;
+# enums
+# constants
+# static variables
+# @export variables
+# remaining regular variables
+# @onready variables
+
+# _static_init()
+# remaining static methods
+# overridden built-in virtual methods:
+#    _init()
+#    remaining virtual methods
+# overridden custom methods
+# remaining public methods
+func enter() -> void:
+	pass;
+
+func exit() -> void:
+	pass;
+
+func update(_delta: float) -> void:
+	pass;
+
+func physics_update(_delta: float) -> void:
+	pass;
+# remaining private methods
+# remaining signal callbacks
+# inner classes
---
+++ src/components/state/state_machine.gd
@@ -0,0 +1,47 @@
+# @tool, @icon, @static_unload
+class_name StateMachine
+extends RefCounted
+# ## doc comment
+
+# signals
+# enums
+# constants
+# static variables
+# @export variables
+# remaining regular variables
+var state_stack: Array[State] = [IdleState.new()]
+
+# @onready variables
+
+# _static_init()
+# remaining static methods
+# overridden built-in virtual methods:
+#    _init()
+#    remaining virtual methods
+# overridden custom methods
+# remaining public methods
+func update_current_state(delta: float) -> void:
+	self.state_stack[-1].update(delta);
+
+func physics_update_current_state(delta: float) -> void:
+	self.state_stack[-1].physics_update(delta);
+
+func push_state(new_state: State) -> void:
+	self.state_stack.push_back(new_state);
+	self._enter_top_state();
+
+# remaining private methods
+func _enter_top_state() -> void:
+	self.state_stack[-1].enter();
+	if self.state_stack[-1].leave_state.connect(_on_current_state_leave_state):
+		"Print Current State: Leave State connection error"
+
+
+# remaining signal callbacks
+func _on_current_state_leave_state() -> void:
+	var last_state: State = self.state_stack.pop_back();
+	last_state.exit();
+	last_state.leave_state.disconnect(_on_current_state_leave_state);
+	self._enter_top_state();
+
+# inner classes
