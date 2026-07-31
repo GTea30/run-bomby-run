f2212f3a73208abeb3ad2624622a77aa79628e13
GTea <grace.tveit@gmail.com>
2026-07-31 12:46:39 -0700

    updating version number

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 project_name="run-bomby-run"
-version="0.2.0"
+version="0.3.0"
 rm -r ./out
 mkdir ./out
 mkdir ./out/web
--- docs/patch_notes.md
+++ docs/patch_notes.md
@@ -1,4 +1,4 @@
-# 0.2.0
+# 0.3.0
 ## Features
 - Enemies now get faster after every round
 - Movement fixed to a grid
